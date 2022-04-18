0e71f7769ac975bc837a75ce7b50001563cbb811
Vrexal <103725404+Vrexal@users.noreply.github.com>
2022-04-18 20:01:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-function compute()
-{
-    var jumlah = document.getElementById("jumlah").value;
+function compute() {
+    var amount = document.getElementById("amount").value;
     var rate = document.getElementById("rate").value;
     var tahun = document.getElementById("tahun").value;
 
-    if (jumlah == "") {
+    if (amount == "") { 
         alert("Amount can't by empty or alphabet character");
-        document.getElementById("jumlah").focus();
+        document.getElementById("amount").focus();
         return false;
     } else {
-        if (jumlah <= 0) { 
+        if (amount <= 0) { 
             alert("Enter a positive number");
-            document.getElementById("jumlah").focus();
+            document.getElementById("amount").focus();
             return false;
         }
     }
 
 
-    var tahun1 = new Date().getFullYear();
-    var future = parseInt(tahun1, 10) + parseInt(tahun, 10);
-    var interest = jumlah * tahun * rate / 100;
-    var result = "If you deposit <mark>" + jumlah + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + future + "</mark>";
+    var has = new Date().getFullYear(); //Here get the year
+    var future = parseInt(has, 10) + parseInt(tahun, 10);
+    var interest = amount * tahun * rate / 100;
+    var result = "If you deposit <mark>" + amount + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + future + "</mark>";
     document.getElementById("hasil").innerHTML = result;
 }
